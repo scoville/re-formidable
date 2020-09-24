e340685d473116d439cb26416b5252efd51a44b5
Kevin COMBRIAT <kevin@sc0ville.com>
2020-09-24 19:22:19 +0900

    style(hooks): Drop dependencies argument from use1

--- src/Formidable.ml
+++ src/Formidable.ml
@@ -546,7 +546,5 @@ let make (type values error)
 let use ~values ~error ~onSubmit ?onSubmitError () =
   React.useMemo0 (fun () -> make ~values ~error ~onSubmit ?onSubmitError ())
 
-let use1 ~values ~error ~onSubmit ?onSubmitError busters =
-  React.useMemo1
-    (fun () -> make ~values ~error ~onSubmit ?onSubmitError ())
-    busters
+let use1 ~values ~error ~onSubmit ?onSubmitError =
+  React.useMemo1 (fun () -> make ~values ~error ~onSubmit ?onSubmitError ())
